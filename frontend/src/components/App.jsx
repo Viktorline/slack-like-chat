bbfe28c6f78c19bc42d65365630314a8d9a2ea3c
Victor Murzin <viktorlinework@gmail.com>
2023-03-07 06:17:27 +0700

    added bootstrap

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,7 @@
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <h1>Hi!</h1>
+      <h1 className="text-center mb-4">Текст по центру</h1>
     </header>
   </div>
 );
