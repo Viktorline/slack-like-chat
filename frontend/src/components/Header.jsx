a0bc0ac07174d2ea6fb8d0e68749bee00f19ccd4
Victor Murzin <viktorlinework@gmail.com>
2023-03-23 09:00:52 +0700

    login fix, package.json fix, eslint rule fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Button, ButtonGroup, Container, Dropdown, DropdownButton, Navbar,
-} from 'react-bootstrap';
+import { Button, ButtonGroup, Container, Dropdown, DropdownButton, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/index.js';
@@ -22,7 +20,7 @@ const Header = () => {
   return (
     <Navbar className="shadow-sm navbar-expand-lg navbar-light bg-white">
       <Container>
-        <Navbar.Brand as={Link} to="/login">
+        <Navbar.Brand as={Link} to="/">
           Hexlet Chat
         </Navbar.Brand>
         {auth.user ? (
