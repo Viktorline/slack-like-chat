f9351565ad3dd40665795ac07a408ba5d2623679
Victor Murzin <viktorlinework@gmail.com>
2023-03-25 09:05:56 +0700

    fix scroll in channels

--- frontend/src/components/Channels/ChannelsBox.jsx
+++ frontend/src/components/Channels/ChannelsBox.jsx
@@ -14,7 +14,7 @@ const Channels = () => {
   const { t } = useTranslation();
 
   return (
-    <Col className="col-4 col-md-2 border-end pt-3 px-0 bg-light h-100 overflow-auto">
+    <Col className="d-flex flex-column h-100 col-4 col-md-2 border-end pt-3 px-0 bg-light">
       <div className="d-flex justify-content-between align-items-center mb-3 ps-4 pe-2 ">
         <span>
           {' '}
@@ -28,7 +28,10 @@ const Channels = () => {
           <span className="font-weight-bold">+</span>
         </Button>
       </div>
-      <Nav variant="pills" className="d-flex flex-column px-2">
+      <Nav
+        variant="pills"
+        className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+      >
         {channels.length > 0
           && channels.map((channel) => <Channel key={channel.id} channel={channel} />)}
       </Nav>
